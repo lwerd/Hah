6d0c133fdd40bed18f46f3570b18954574cd951d
lwerd <49458324+lwerd@users.noreply.github.com>
2019-04-09 17:10:09 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,10 @@
+$(".space").click(function(){
+    $("#box1").toggle();
+});
+$(".spong").click(function(){
+    $("#box2").fadeToggle();
+});
+$(".rand").click(function(){
+    $("#box3").slideToggle();
+});
+
